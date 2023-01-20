9e40ad99164b5b7e513751b68458361a386ad7bd
fr-mm <chico.zimcolor@gmail.com>
2023-01-20 03:06:15 -0300

    logout implementado

--- src/componentes/app/App.tsx
+++ src/componentes/app/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PaginaEnum from "../../enums/paginaEnum";
 import { reducers, RootState } from "../../store";
@@ -6,38 +7,37 @@ import { Chat, Login } from "../paginas";
 import "./App.css";
 
 function App(): JSX.Element {
-  const pagina = useSelector((state: RootState) => state.pagina.visivel);
   const dispatch = useDispatch();
+  const [carregou, setCarregou] = useState(false);
 
-  function lembrarUsuario(): void {
-    const id = localStorage.getItem("id");
-    const nome = localStorage.getItem("nome");
-    const token = localStorage.getItem("token");
+  useEffect(() => {
+    if (!carregou) {
+      const id = localStorage.getItem("id");
+      const nome = localStorage.getItem("nome");
+      const token = localStorage.getItem("token");
 
-    if (id !== null && nome !== null && token !== null) {
-      dispatch(reducers.usuario.logar({ id, nome, token }));
-      dispatch(reducers.pagina.mostrarChat());
+      if (id !== null && nome !== null && token !== null) {
+        dispatch(reducers.usuario.logar({ id, nome, token }));
+        dispatch(reducers.pagina.mostrarChat());
+      }
+
+      setCarregou(true);
     }
-  }
-
-  lembrarUsuario();
+  }, [carregou, dispatch]);
 
   return (
     <div className="App">
       <NavBar />
       <div className="app-corpo">
-        <Pagina pagina={pagina} />
+        <Pagina />
       </div>
     </div>
   );
 }
 
-interface PaginaProps {
-  pagina: PaginaEnum;
-}
-
-function Pagina(props: PaginaProps): JSX.Element {
-  switch (props.pagina) {
+function Pagina(): JSX.Element {
+  const pagina = useSelector((state: RootState) => state.pagina.visivel);
+  switch (pagina) {
     case PaginaEnum.LOGIN:
       return <Login />;
     case PaginaEnum.CHAT:
--- src/componentes/navBar/NavBar.tsx
+++ src/componentes/navBar/NavBar.tsx
@@ -1,12 +1,39 @@
 import "./NavBar.css";
 import logo from "../../static/logo.png";
+import { useDispatch, useSelector } from "react-redux";
+import { reducers, RootState } from "../../store";
 
 function NavBar(): JSX.Element {
   return (
     <div className="nav-bar">
       <img src={logo} alt="SaltZapp" />
+      <div className="filler"></div>
+      <MenuUsuario />
     </div>
   );
+}
+
+function MenuUsuario(): JSX.Element {
+  const usuario = useSelector((state: RootState) => state.usuario);
+  const dispatch = useDispatch();
+
+  function sair(): void {
+    dispatch(reducers.usuario.sair());
+    dispatch(reducers.pagina.mostrarLogin());
+  }
+
+  if (usuario.id !== null) {
+    return (
+      <div className="menu-usuario">
+        <div className="nome-usuario-logado">{usuario.nome}</div>
+        <div className="sair" onClick={sair}>
+          (sair)
+        </div>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 }
 
 export default NavBar;
